dd6ca1c094c11d7ef621f35e54024d88e145b2c2
dqhuy78 <dqhuy78@gmail.com>
2019-01-17 17:40:41 +0700

    Update some new color

--- src/components/Color/Box/index.js
+++ src/components/Color/Box/index.js
@@ -4,10 +4,9 @@ import './style.scss';
 
 const Box = ({ color }) => (
     <div className='Box'>
-        <div className='Box__color' style={{ backgroundColor: color.color }}>
-            {color.color}
+        <div className='Box__color' style={{ backgroundColor: color }}>
+            {color}
         </div>
-        <p className='Box__title'>{color.title}</p>
     </div>
 )
 
--- src/components/Color/index.js
+++ src/components/Color/index.js
@@ -5,21 +5,51 @@ import './style.scss';
 import Box from './Box/';
 
 const colors = [
-    { title: 'Red', color: '#e8384f' },
-    { title: 'Orange', color: '#fd612c' },
-    { title: 'Yellow Orange', color: '#fd9a00' },
-    { title: 'Yellow', color: '#eec300' },
-    { title: 'Yellow Green', color: '#a4cf30' },
-    { title: 'Green', color: '#62d26f' },
-    { title: 'Blue Green', color: '#37c5ab' },
-    { title: 'Aqua', color: '#20aaea' },
-    { title: 'Blue', color: '#4186e0' },
-    { title: 'Indigo', color: '#7a6ff0' },
-    { title: 'Purple', color: '#aa62e3' },
-    { title: 'Magenta', color: '#e362e3' },
-    { title: 'Hot Pink', color: '#ea4e9d' },
-    { title: 'Pink', color: '#fc91ad' },
-    { title: 'Cool Gray', color: '#8da3a6' },
+    {
+        title: 'Red', content: [
+            '#e8384f', '#ff6565', '#ff2f2f', '#fa5e5b'
+        ]
+    },
+    {
+        title: 'Green', content: [
+            '#16c98d', '#0fdebd', '#37c5ab', '#25c58c', '#62d26f', '#a4cf30'
+        ]
+    },
+    {
+        title: 'Blue', content: [
+            '#142b44', '#1d508d', '#297cbb', '#288ad6', '#4186e0', '#427eff', '#20aaea'
+        ]
+    },
+    {
+        title: 'Orange', content: [
+            '#fd612c', '#ff8104', '#fd9a00', '#ffc83f'
+        ]
+    },
+    {
+        title: 'Yellow', content: [
+            '#ffba0f', '#eec300', '#feef6d'
+        ]
+    },
+    {
+        title: 'Pink', content: [
+            '#ea4e9d', '#e362e3', '#ff33ed', '#fc91ad', '#bf538d'
+        ]
+    },
+    {
+        title: 'Purple', content: [
+            '#7a6ff0', '#aa62e3',
+        ]
+    },
+    {
+        title: 'Gray', content: [
+            '#666666', '#9a9a9a', '#8da3a6', '#4b515d', '#3e4551', '#263238', '#37474f'
+        ]
+    },
+    {
+        title: 'Black', content: [
+            '#212121', '#2e2e2e',
+        ]
+    }
 ];
 
 const Color = () => (
@@ -27,17 +57,26 @@ const Color = () => (
         <h2 className='Color__title'>
             Main Color
         </h2>
-        <div className='Color__container'>
-            {
-                colors.map(color => (
-                    <div key={color.color} className='Color__item'>
-                        <Link to='/' className='Color__link'>
-                            <Box color={color} />
-                        </Link>
+        {
+            colors.map(color => (
+                <div key={color.title} className='Color__group'>
+                    <div className='Color__group-title'>
+                        {color.title}
                     </div>
-                ))
-            }
-        </div>
+                    <div className='Color__container'>
+                        {
+                            color.content.map(color => (
+                                <div key={color} className='Color__item'>
+                                    <Link to='/' className='Color__link'>
+                                        <Box color={color} />
+                                    </Link>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
+            ))
+        }
     </div>
 )
 
